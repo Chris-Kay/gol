19bc674a766de2603a8fde8e5db8d07d31c49885
Iain Tatch <iain.tatch@bbc.co.uk>
2015-04-10 20:05:45 +0100

    Working vagrantfile & provisioning. Plus a nice script to tie it all together

--- perl/install/vagrant.sh
+++ perl/install/vagrant.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
+__box_exists=$( vagrant box list | grep virtualbox/ubuntu/trusty-server )
+
+if [[ -z $__box_exists ]]; then
+
+  set -x
+  vagrant box add virtualbox/ubuntu/trusty-server https://oss-binaries.phusionpassenger.com/vagrant/boxes/latest/ubuntu-14.04-amd64-vbox.box
+  set +x
+fi
+
 set -x
 
-vagrant box add virtualbox/ubuntu/trusty-server https://oss-binaries.phusionpassenger.com/vagrant/boxes/latest/ubuntu-14.04-amd64-vbox.box
-vagrant init virtualbox/ubuntu/trusty-server
 vagrant up
+vagrant provision
+vagrant ssh -c /vagrant/install-moose.sh
+
+set +x
+
+echo
+echo '************************************************************************'
+echo
+echo 'About to connect to vagrant host'
+echo
+echo 'To confirm that environment is set correctly, do:'
+echo '   $ /vagrant/env_check.sh'
+echo
+echo 'To run the perl test suite, do:'
+echo '   $ cd ~/coding-test'
+echo '   $ ./run_tests.sh'
+echo
+echo '************************************************************************'
+echo
+
+set -x
+
 vagrant ssh
 
