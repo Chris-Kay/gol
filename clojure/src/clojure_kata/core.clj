ee863c6e299ac57ac52160689ccffdd1b84923de
Hakim.Cassimally <hakim.cassimally@bbc.co.uk>
2017-05-25 17:19:14 +0100

    Clojure stub
    I've not replicated the Life grid / cell classes from other language
    versions, as they don't really make sense for an idiomatic Clojure
    style.  Instead, I've just stubbed 'evolve-grid' in a comment to hint
    that it could be done with a set.

---
+++ clojure/src/clojure_kata/core.clj
@@ -0,0 +1,14 @@
+(ns clojure-kata.core
+  (:gen-class))
+
+(defn cell-should-survive [num-neighbours]
+  (throw (Exception. "Unsupported operation")))
+
+;; Later...
+;; (defn evolve-grid [live-cell-set]
+;;  #{})
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
