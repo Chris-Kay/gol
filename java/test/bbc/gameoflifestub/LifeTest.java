1c0a3b8869809600fb2cc0e1d14c2fed8c95d22e
Peter Newman <peter.newman@bbc.co.uk>
2018-05-25 09:19:28 +0100

    Fix indentation of test file
    The indentation of the LifeTest.java file was incorrect, with mixed
    indentation.

--- java/test/bbc/gameoflifestub/LifeTest.java
+++ java/test/bbc/gameoflifestub/LifeTest.java
@@ -7,25 +7,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
-	
+
 public class LifeTest {
 
-	protected Set<Cell> setOfCells;
-	protected Life life;
-	
-	@Before
-	public void setUp(){
-    	setOfCells = new HashSet<Cell>();
-    	setOfCells.add(new Cell(1,1));
-    	setOfCells.add(new Cell(2,2));
+    protected Set<Cell> setOfCells;
+    protected Life life;
+
+    @Before
+    public void setUp(){
+        setOfCells = new HashSet<Cell>();
+        setOfCells.add(new Cell(1,1));
+        setOfCells.add(new Cell(2,2));
         life = new Life(setOfCells);
     }
-	
-	@Test
-	public void testInitialisation()
-	{
-		assertEquals(2, life.getLiveCells().size());
-	}
+
+    @Test
+    public void testInitialisation()
+    {
+        assertEquals(2, life.getLiveCells().size());
+    }
 
     @Test
     public void testUnderpopulation()
