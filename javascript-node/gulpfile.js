6d61efb5a708e251fa65bff84ca352868d5c0133
Andrew Hillel <subvertnormality@users.noreply.github.com>
2018-06-18 14:40:39 +0100

    Linting should not be default behaviour

--- javascript-node/gulpfile.js
+++ javascript-node/gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var jshint = require('gulp-jshint');
 
-gulp.task('default', ['lint','test']);
+gulp.task('default', ['test']);
 
-gulp.task('test', ['lint'], function() {
+gulp.task('test', function() {
 	return gulp.src('test/*.js', {read: false})
 		.pipe(mocha());
 });
